f38a39160bfee67471adb30d84e9663877a3d716
Marcel Stadelmann <m@marceli.to>
2025-01-21 19:13:49 +0100

    wip

--- app/Notifications/InquiryOwnerEmail.php
+++ app/Notifications/InquiryOwnerEmail.php
@@ -41,7 +41,7 @@ class InquiryOwnerEmail extends Notification
     return (new MailMessage)
       ->from(env('MAIL_FROM_ADDRESS'))
       ->replyTo(env('MAIL_TO'))
-      ->subject('Interessent ruetlistrasse-effretikon.ch')
+      ->subject('Interessent creativefactory-renens.ch')
       ->markdown('mail.inquiry.owner', ['data' => $this->data]);
   }
 
